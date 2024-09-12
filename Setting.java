92ce91661d81595fe4947d06e67af4a993383f91
Sam Wells <swells1@mines.edu>
2024-09-12 16:10:32 -0600

    Added enum type with value and toString()

--- Setting.java
+++ Setting.java
@@ -1,1 +1,14 @@
+package Stove;
 
+public enum Level {
+  OFF ("---"), LOW ("--+"), MEDIUM ("-++"), HIGH ("+++");
+  private String value;
+
+  Level(String inVal) {
+    value = inVal;
+  }
+
+  public String toString() {
+    return value;
+  }
+}
