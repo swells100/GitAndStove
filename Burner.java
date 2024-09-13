dbd32a25fd9c03fb8d8ef706a9dea4315952aedc
daghassi <79544966+daghassi@users.noreply.github.com>
2024-09-13 12:46:50 -0600

    burner

--- Burner.java
+++ Burner.java
@@ -5,7 +5,48 @@ public class Burner {
 	Setting mySetting;
 	int timer;
 	public final int TIME_DURATION; 
+	public Burner() {
+		myTempreture = COLD;
+		mySetting = OFF;
+	}
 	public Temperature getMyTempreture() {
 		return myTempreture;
 	}
+	public void plusButton() {
+		timer = TIME_DURATION;
+		switch(getMyTempreture()) {
+		  case COLD:
+		    myTempreture = WARM;
+		    break;
+		  case WARM:
+		    myTempreture = HOT:
+		    break;
+		  case HOT:
+			myTempreture = BLAZING;
+		  default:
+			break;
+		}
+	}
+	public void minusButton() {
+		timer = TIME_DURATION;
+		switch(getMyTempreture()) {
+		  case BLAZING:
+		    myTempreture = HOT;
+		    break;
+		  case HOT:
+		    myTempreture = WARM:
+		    break;
+		  case COLD:
+			myTempreture = OFF;
+		  default:
+			break;
+		}
+	}
+	public void updateTimer() {
+		timer++;
+	}
+	public void display() {
+		System.out.println(Burner.toString());
+	}
+	
 }
